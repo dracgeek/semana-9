fd34b4617552626e8b133c65beafb3517232efe9
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2020-10-13 00:31:53 -0500

    first commit

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'base_datos_semana_8',
+  multipleStatements: true
+});
+mysqlConnection.connect(function (err) {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    console.log('base de datos conectada!');
+  }
+});
+
+module.exports = mysqlConnection;
